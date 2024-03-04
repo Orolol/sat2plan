ba2e4622bdf17b2fe6b99cac669e9a795cb18387
louishenri baudeylaubier <louishenrilewagon@gmail.com>
2024-03-04 16:33:03 +0100

    Resize image

--- sat2plan/logic/preproc/data_preparation.py
+++ sat2plan/logic/preproc/data_preparation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from torchvision.io import read_image
+from torchvision import transforms
 from torch import tensor
 import os
 
@@ -21,3 +22,18 @@ def spliting_image(path):
         y.append(img[:,:,512:])
 
     return X, y
+
+def resize(image,pourcentage=0.5):
+    """
+    Cette fonction accepte en entrée une liste de tenseur d'images.
+    La modification de la taille s'effectue à l'aide de la variable "pourcentage" qui est de 50% par défaut
+    On obtient en sortie une liste de tenseur d'images
+    """
+    # Initialisation de la liste de sortie
+    image_resize = []
+
+    # Boucle itérative du post-traitement
+    for img in image:
+        image_resize.append(transforms.Resize(int(img.size()[1] * pourcentage))(img))
+
+    return image_resize

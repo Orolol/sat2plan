9ab5cb7b3df690505ef05938fa7c16586592590e
NicolasD <nicolas.destrac@gmail.com>
2024-03-06 12:55:47 +0100

    config et building modifiés

--- sat2plan/logic/models/model_building.py
+++ sat2plan/logic/models/model_building.py
@@ -1,81 +1,138 @@
-import torch.nn as nn
 import torch
-
-#Initialisation des poids
-def weights_init_normal(m):
-    classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm2d") != -1:
-        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
-        torch.nn.init.constant_(m.bias.data, 0.0)
+import torch.nn as nn
 
 
-#Discriminateur
+
+
+####################################################################################################################
+################################################## DISCRIMINATEUR ##################################################
+####################################################################################################################
+
+
+class CNN_Block(nn.Module):
+    def __init__(self,in_channels,out_channels, kernel_size, stride, padding):
+        super().__init__()
+        self.conv = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False, padding_mode="reflect"),
+                nn.BatchNorm2d(out_channels),
+                nn.LeakyReLU(0.2)
+            )
+    def forward(self, x):
+        return self.conv(x)
+
 class Discriminator(nn.Module):
-    def __init__(self, img_size=256, kernel_size=3, stride=1, padding=1, channels=3):
-        self.img_size = img_size
-        super(Discriminator, self).__init__()
-
-        def discriminator_block(in_filters, out_filters, bn=True):
-            block = [nn.Conv2d(in_filters, out_filters, kernel_size, stride, padding), nn.LeakyReLU(
-                0.2, inplace=True), nn.Dropout2d(0.25)]
-            if bn:
-                block.append(nn.BatchNorm2d(out_filters, 0.8))
-            return block
-
-        self.model = nn.Sequential(
-            *discriminator_block(channels, 16, bn=False),
-            *discriminator_block(16, 32),
-            *discriminator_block(32, 64),
-            *discriminator_block(64, 128),
+    def __init__(self, kernel_size, stride, padding, in_channels=3, features = [64,128,256,512]):
+        super().__init__()
+        self.initial = nn.Sequential(
+            nn.Conv2d(in_channels*2, features[0], kernel_size, stride, padding, padding_mode="reflect"),
+            nn.LeakyReLU(0.2)
         )
 
-        # The height and width of downsampled image
-        ds_size = img_size // 2 ** 4
-        self.adv_layer = nn.Sequential(
-            nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+        layers = []
+        in_channels = features[0]
+        for feature in features[1:]:
+            layers.append(
+                CNN_Block(in_channels, feature, stride=1 if feature==features[-1] else stride)
 
-    def forward(self, img):
-        out = self.model(img)
-        out = out.view(out.shape[0], -1)
-        validity = self.adv_layer(out)
+            )
+            in_channels = feature
+        layers.append(
+            nn.Conv2d(
+                in_channels, 1, kernel_size, stride=1, padding = padding, padding_mode="reflect"
+            )
+        )
 
-        return validity
+        self.model = nn.Sequential(*layers)
+
+    def forward(self,x,y):
+        ### X = Correct Satellite Image
+        ### Y = Correct/Fake Image
+
+        x = torch.cat([x,y],dim=1)
+        x = self.initial(x)
+        return self.model(x)
 
 
-#Générateur
+
+####################################################################################################################
+#################################################### GENERATEUR ####################################################
+####################################################################################################################
+
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels,  kernel_size, stride, padding, down = True, act="relu", use_dropout=False):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False, padding_mode="reflect")
+            if down
+            else nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU() if act=="relu" else nn.LeakyReLU(0.2),
+        )
+        self.use_dropout = use_dropout
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self,x):
+        x = self.conv(x)
+        return self.dropout(x) if self.use_dropout else x
+
 class Generator(nn.Module):
-    def __init__(self, img_size=256, latent_dim=100, kernel_size=3, stride=1, padding=1, channels=3):
-        super(Generator, self).__init__()
+    def __init__(self,  kernel_size, stride, padding, in_channels=3,features=64):
+        super().__init__()
+        self.initial_down = nn.Sequential(
+            nn.Conv2d(in_channels, features, kernel_size, stride, padding, padding_mode="reflect"),
+            nn.LeakyReLU(0.2)
+        )  # 128 X 128
 
-        # self.init_size = img_size // 4
-        self.init_size = img_size
-
-        self.l1 = nn.Sequential(
-            nn.Linear(latent_dim, 128 * self.init_size ** 2))
-
-        self.conv_blocks = nn.Sequential(
-            # nn.Upsample(scale_factor=2),
-            nn.Conv2d(channels, 128, kernel_size, stride, padding),
-            nn.BatchNorm2d(128, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(128, 128, kernel_size, stride, padding),
-            nn.BatchNorm2d(128, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            # nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 64, kernel_size, stride, padding),
-            nn.BatchNorm2d(64, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, channels, kernel_size, stride, padding),
-            nn.Tanh(),
+        ##############################################################################
+        ################################## ENCODEUR ##################################
+        ##############################################################################
+        self.down1 = Block(features, features*2, down=True, act="leaky", use_dropout=False)    # 64 X 64
+        self.down2 = Block(features*2, features*4, down=True, act="leaky", use_dropout=False)  # 32 X 32
+        self.down3 = Block(features*4, features*8, down=True, act="leaky", use_dropout=False)  # 16 X 16
+        self.down4 = Block(features*8, features*8, down=True, act="leaky", use_dropout=False)  # 8 X 8
+        self.down5 = Block(features*8, features*8, down=True, act="leaky", use_dropout=False)  # 4 X 4
+        self.down6 = Block(features*8, features*8, down=True, act="leaky", use_dropout=False)  # 2 X 2
+        ##############################################################################
+        ################################# BOTTLENECK #################################
+        ##############################################################################
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(features*8,features*8, kernel_size, stride, padding,padding_mode="reflect"),                      # 1 X 1
+            nn.ReLU()
+        )
+        ##############################################################################
+        ################################## DECODEUR ##################################
+        ##############################################################################
+        self.up1 = Block(features*8, features*8, down=False, act="relu", use_dropout=True)
+        self.up2 = Block(features*8*2, features*8, down=False, act="relu", use_dropout=True)
+        self.up3 = Block(features*8*2, features*8, down=False, act="relu", use_dropout=True)
+        self.up4 = Block(features*8*2, features*8, down=False, act="relu", use_dropout=False)
+        self.up5 = Block(features*8*2, features*4, down=False, act="relu", use_dropout=False)
+        self.up6 = Block(features*4*2, features*2, down=False, act="relu", use_dropout=False)
+        self.up7 = Block(features*2*2, features, down=False, act="relu", use_dropout=False)
+        self.final_up = nn.Sequential(
+            nn.ConvTranspose2d(features*2, in_channels, kernel_size, stride, padding),
+            nn.Tanh()
         )
 
-    def forward(self, z):
-        # print("Z", z.shape)
-        # out = self.l1(z)
-        # out = z.view(z.shape[0], 128, self.init_size, self.init_size)
-        # print("OUT", z.shape)
-        img = self.conv_blocks(z)
-        # print("IMG", img.shape)
-        return img
+
+    def forward(self,x):
+        d1 = self.initial_down(x)
+        d2 = self.down1(d1)
+        d3 = self.down2(d2)
+        d4 = self.down3(d3)
+        d5 = self.down4(d4)
+        d6 = self.down5(d5)
+        d7 = self.down6(d6)
+
+        bottleneck = self.bottleneck(d7)
+
+        up1 = self.up1(bottleneck)
+        up2 = self.up2(torch.cat([up1, d7], 1))
+        up3 = self.up3(torch.cat([up2, d6], 1))
+        up4 = self.up4(torch.cat([up3, d5], 1))
+        up5 = self.up5(torch.cat([up4, d4], 1))
+        up6 = self.up6(torch.cat([up5, d3], 1))
+        up7 = self.up7(torch.cat([up6, d2], 1))
+
+        return self.final_up(torch.cat([up7, d1],1))

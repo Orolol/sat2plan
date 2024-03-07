da46c4e97bb5a030fd224938052e783db6b56c27
Nils Coupey <coupey.nils@gmail.com>
2024-03-07 10:53:50 +0100

    Split de la config et création de la classe Unet

--- sat2plan/interface/main.py
+++ sat2plan/interface/main.py
@@ -7,6 +7,7 @@ from dateutil.parser import parse
 
 from sat2plan.logic.preproc.data import download_bucket_folder
 from sat2plan.logic.models.unet.model_training import train_model
+from sat2plan.logic.models.unet.model_training_class import Model_Training
 
 
 # @mlflow_run
@@ -18,7 +19,9 @@ def train():
     download_bucket_folder(data_bucket, val_size=0.1)
 
     print("Running unet training")
-    train_model(data_bucket=data_bucket)
+    #train_model(data_bucket=data_bucket)
+    unet = Model_Training(data_bucket=data_bucket)
+    unet.train()
 
 
 def pred():

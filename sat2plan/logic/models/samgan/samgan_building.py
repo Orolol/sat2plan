ac1714591c3642af9dc3649e511fc183e1360685
NicolasD <nicolas.destrac@gmail.com>
2024-03-08 16:44:54 +0100

    modif erreur

--- sat2plan/logic/models/samgan/samgan_building.py
+++ sat2plan/logic/models/samgan/samgan_building.py
@@ -82,7 +82,7 @@ class SAM_GAN(nn.Module):
         return y_fake
 
 class Discriminator(nn.Module):
-    def __init__(self, kernel_size=4, stride=2, padding=1, in_channels=3, features=[64, 128, 256, 512]):
+    def __init__(self, kernel_size=4, stride=2, padding=1, in_channels=3, features=[64, 128, 256]):
         super().__init__()
         self.initial = nn.Sequential(
             nn.Conv2d(

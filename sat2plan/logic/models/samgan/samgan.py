b8d706db54e53d92013b3d0dd495f3ab0c0dca67
NicolasD <nicolas.destrac@gmail.com>
2024-03-08 15:53:14 +0100

    modif erreur

--- sat2plan/logic/models/samgan/samgan.py
+++ sat2plan/logic/models/samgan/samgan.py
@@ -160,7 +160,7 @@ class SAMGAN():
                 ############## Train Discriminator ##############
 
                 # Measure discriminator's ability to classify real from generated samples
-                y_fake = self.sam_gan(x)
+                y_fake = self.sam_gan(x, y)
                 D_real = self.netD(x, y)
                 D_real_loss = self.BCE_Loss(D_real, torch.ones_like(D_real))
                 D_fake = self.netD(x, y_fake.detach())

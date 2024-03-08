99cfd0286c80ec9b299ec919aeef048a82f7bf3a
NicolasD <nicolas.destrac@gmail.com>
2024-03-08 16:51:02 +0100

    modif erreur

--- sat2plan/logic/models/samgan/samgan.py
+++ sat2plan/logic/models/samgan/samgan.py
@@ -178,7 +178,7 @@ class SAMGAN():
                 # Loss measures generator's ability to fool the discriminator
                 D_fake = self.netD(x, y_fake)
                 G_fake_loss = self.BCE_Loss(D_fake, torch.ones_like(D_fake))
-                print(G_fake_loss.shape)
+                print(G_fake_loss.size())
                 G_loss = G_fake_loss + self.content_loss(y_fake, y) + self.style_loss(y_fake,y)
                 self.Gen_loss.append(G_loss.item())
 

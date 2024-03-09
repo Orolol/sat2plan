ceeb037718a884bf51fcafd0d9f32b7e01364f52
NicolasD <nicolas.destrac@gmail.com>
2024-03-09 22:58:20 +0100

    modif samgan

--- sat2plan/logic/models/samgan/samgan.py
+++ sat2plan/logic/models/samgan/samgan.py
@@ -178,7 +178,7 @@ class SAMGAN():
                 # Loss measures generator's ability to fool the discriminator
                 D_fake = self.netD(x, y_fake)
                 G_fake_loss = self.BCE_Loss(D_fake, torch.ones_like(D_fake))"""
-                Sam_gan_loss = self.adversarial_loss(y_fake, y) + self.content_loss(y_fake, y) + self.style_loss(y_fake,y)
+                Sam_gan_loss = self.adversarial_loss(y_fake.detach(), y) + self.content_loss(y_fake, y) + self.style_loss(y_fake,y)
                 self.Gen_loss.append(Sam_gan_loss.item())
 
                 # Backward and optimize

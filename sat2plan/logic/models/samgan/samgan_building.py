ce6d60bce818898f96234fbe1d3c4ed96e5bd58f
NicolasD <nicolas.destrac@gmail.com>
2024-03-09 19:16:58 +0100

    modif samgan

--- sat2plan/logic/models/samgan/samgan_building.py
+++ sat2plan/logic/models/samgan/samgan_building.py
@@ -49,7 +49,7 @@ class PCIR(nn.Module):
         return x
 
 class CTIR(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super(CTIR, self).__init__()
 
         self.conv_transpose = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding)

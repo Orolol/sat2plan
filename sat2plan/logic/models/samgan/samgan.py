897cd24ff6374291cae5beefc831b81194ff31d5
NicolasD <nicolas.destrac@gmail.com>
2024-03-13 12:48:09 +0100

    cleaning

--- sat2plan/logic/models/samgan/samgan.py
+++ sat2plan/logic/models/samgan/samgan.py
@@ -150,7 +150,6 @@ class SAMGAN():
 
                 # Measure discriminator's ability to classify real from generated samples
                 y_fake = self.sam_gan(x, y)
-                print(to_save[0])
                 if to_save[0]:
                     save_image(y_fake, f"save/y_gen_{epoch}_{idx}.png")
                     save_image(x, f"save/input_{epoch}_{idx}.png")
